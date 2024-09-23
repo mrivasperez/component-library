ad7e93cea22149535c993e749af328976847314e
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-23 13:59:50 -0700

    style: Dropdown styling

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+
 export type DropdownOption = { label: string; value: string };
 export type DropdownOptionList = DropdownOption[];
 
@@ -11,33 +12,38 @@ interface DropdownProps {
 const Dropdown = ({ options, value, onChange }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const handleOpenDropdown = () => {
+  const handleClick = () => {
     setIsOpen(!isOpen);
   };
 
   const handleSelectOption = (option: { label: string; value: string }) => {
-    handleOpenDropdown();
+    handleClick();
     onChange(option);
   };
 
-  const handleClearSelection = () => onChange(null);
-
   const renderedOptions = options.map((option) => {
     return (
-      <div onClick={() => handleSelectOption(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleSelectOption(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
 
   return (
-    <div>
-      <div onClick={handleOpenDropdown}>
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer border rounded p-2 shadow bg-white w-full"
+        onClick={handleClick}
+      >
         {value ? value.label : "Select..."}
+        <span>⯆</span>
       </div>
       {isOpen && (
-        <div>
-          {value && <div onClick={handleClearSelection}>Clear</div>}
+        <div className="absolute top-full border rounded p-2 bg-white shadow w-full">
           {renderedOptions}
         </div>
       )}
---
+++ src/components/Panel.tsx
@@ -0,0 +1,7 @@
+import classNames from "classnames";
+
+const Panel = () => {
+  return <div>Panel</div>;
+};
+
+export default Panel;
