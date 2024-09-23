327aec2f4c530de812f8d30259ea1530d83f05c9
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-23 14:54:42 -0700

    task: Create DropdownPage component

---
+++ src/pages/DropdownPage.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+import Dropdown, { DropdownOption, DropdownOptionList } from "../components/Dropdown";
+
+const DropdownPage = () => {
+  const [selection, setSelection] = useState<null | DropdownOption>(null);
+
+  const handleSelect = (option: DropdownOption | null) => {
+    return setSelection(option);
+  };
+
+  const options: DropdownOptionList = [
+    { label: "Red", value: "red" },
+    { label: "Yellow", value: "yellow" },
+    { label: "Blue", value: "blue" }
+  ];
+
+  return (
+    <Dropdown options={options} value={selection} onChange={handleSelect} />
+  );
+};
+
+export default DropdownPage;
