947181c0ecc96dbb7d2b915550c59998c635f790
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-23 10:15:09 -0700

    style: Add rounded, outline variants to style

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,16 @@ function App() {
       <Button variant="secondary">Hello!</Button>
       <Button variant="success">Hello!</Button>
       <Button variant="warning">Hello!</Button>
-      <Button />
+      <Button rounded>Hello!</Button>
+      <Button variant="danger" rounded>Hello!</Button>
+      <Button variant="secondary" rounded>Hello!</Button>
+      <Button variant="success" rounded>Hello!</Button>
+      <Button variant="warning" rounded>Hello!</Button>
+      <Button outline>Hello!</Button>
+      <Button variant="danger" outline>Hello!</Button>
+      <Button variant="secondary" outline>Hello!</Button>
+      <Button variant="success" outline>Hello!</Button>
+      <Button variant="warning" outline>Hello!</Button>
     </div>
   );
 }
--- src/Button.tsx
+++ src/Button.tsx
@@ -14,14 +14,21 @@ const Button = ({
   outline
 }: PropsWithChildren<ButtonProps>) => {
   // TODO create classNames
-  const buttonClassNames = classNames("px-3 py-1.5 border", {
+  const buttonClassNames = classNames("px-4 py-1.5 border", {
     "text-white": variant !== "secondary" && variant !== "warning",
     "bg-red-600 border-red-600": variant === "danger",
     "bg-blue-600 border-blue-600":
       variant === "primary" || variant === undefined,
-    "bg-gray-100 border-gray-200": variant === "secondary",
+    "bg-gray-200 border-gray-300": variant === "secondary",
     "bg-green-600 border-green-600": variant === "success",
-    "bg-yellow-400 border-yellow-400": variant === "warning"
+    "bg-yellow-400 border-yellow-400": variant === "warning",
+    "rounded-full": rounded,
+    "bg-white": outline,
+    "text-red-800": outline && variant === "danger",
+    "text-blue-700": outline && variant === "primary" || outline && variant === undefined,
+    "text-gray-800": outline && variant === "secondary",
+    "text-green-800": outline && variant === "success",
+    "text-yellow-800": outline && variant === "warning",
   });
 
   return (
