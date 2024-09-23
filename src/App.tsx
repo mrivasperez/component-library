5fdbb5083d574ab051c8026463d26bce1df65605
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-23 11:07:49 -0700

    feat: Create accordion component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import Accordion, { AccordionItems } from "./components/Accordion";
+
 function App() {
-  return <h1></h1>;
+  const accordionItems: AccordionItems = [
+    {
+      id: "1",
+      label: "What is React?",
+      content:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Faucibus cursus volutpat odio nisl mi scelerisque turpis semper."
+    },
+    {
+      id: "3",
+      label: "What is TypeScript?",
+      content:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Faucibus cursus volutpat odio nisl mi scelerisque turpis semper."
+    },
+    {
+      id: "25",
+      label: "What is the meaning of life?",
+      content:
+        "Lorem ipsum odor amet, consectetuer adipiscing elit. Faucibus cursus volutpat odio nisl mi scelerisque turpis semper."
+    }
+  ];
+  return <Accordion items={accordionItems} />;
 }
 
 export default App;
---
+++ src/components/Accordion.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export type AccordionItems = { id: string; label: string; content: string }[];
+
+interface AccordionProps {
+  items: AccordionItems;
+}
+
+const Accordion = ({ items }: AccordionProps) => {
+  const [expandedItemIndex, setExpandedItemIndex] = useState<
+    undefined | number
+  >();
+
+  const handleClickItem = (clickedItemIndex: number) => {
+    if (clickedItemIndex === expandedItemIndex) return setExpandedItemIndex(undefined);
+    else return setExpandedItemIndex(clickedItemIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedItemIndex;
+    const icon = <span>{isExpanded ? '⯅' : "⯆" }</span>
+    return (
+      <div key={item.id}>
+        <div onClick={() => handleClickItem(index)}>{icon}{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
+  });
+
+  return <div>{renderedItems}</div>;
+};
+
+export default Accordion;
