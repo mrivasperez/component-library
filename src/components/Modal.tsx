b287f5f03faf19599a53a15157505bd9ead0266e
Miguel Rivas Perez <mrivasperez@icloud.com>
2024-09-25 09:27:56 -0700

    task: Create portal for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import ReactDOM from "react-dom";
 
 const Modal = () => {
-  return <div>Modal</div>;
+  return ReactDOM.createPortal(
+    <div>
+      <div className=" absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores,
+        numquam.
+      </div>
+    </div>,
+    document.getElementById("modal-container")!
+  );
 };
 
 export default Modal;
